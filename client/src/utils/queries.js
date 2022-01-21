2e84751237099d5702eaed75896ccb582ae8be02
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-21 11:35:29 -0500

    LoginForm component has now been setup with Apollo and GraphQL and is working for now

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,19 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_ME = gql`
-
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            _id
+            bookId
+            authors
+            description
+            image
+            link
+            title
+        }
+  }
 `;
